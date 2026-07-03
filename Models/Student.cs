22596d11d041967b8242ec45e626a1bdcab72188
Bhavyang Dixit <dixitbhavyang111@gmail.com>
2026-07-03 14:20:58 +0530

    Add Student model

---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBDemo.Models
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+
+        [BsonElement("city")]
+        public string City { get; set; } = null!;
+
+        [BsonElement("score")]
+        public double Score { get; set; }
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; }
+    }
+}
